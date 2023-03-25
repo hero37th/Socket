a7b274b35708715c9d011ac8e23ed4adfbb8b7e9
hero37th <49057215+hero37th@users.noreply.github.com>
2023-03-25 14:00:40 +0800

    Add files via upload

---
+++ TcpClient.cc
@@ -0,0 +1,55 @@
+#include "TcpClient.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Initialize socket.
+    TCPSocket tcpSocket([](int errorCode, std::string errorMessage){
+        cout << "Socket creation error:" << errorCode << " : " << errorMessage << endl;
+    });
+
+    // Start receiving from the host.
+    tcpSocket.onMessageReceived = [](string message) {
+        cout << "Message from the Server: " << message << endl;
+    };
+    // If you want to use raw bytes instead of std::string:
+    /*
+    tcpSocket.onRawMessageReceived = [](const char* message, int length) {
+        cout << "Message from the Server: " << message << "(" << length << ")" << endl;
+    };
+    */
+    
+    // On socket closed:
+    tcpSocket.onSocketClosed = [](int errorCode){
+        cout << "Connection closed: " << errorCode << endl;
+    };
+
+    // Connect to the host.
+    tcpSocket.Connect("127.0.0.1", 8889, [&] {
+        cout << "Connected to the server successfully." << endl;
+
+        // Send String:
+        tcpSocket.Send("Hello Server!");
+    },
+    [](int errorCode, std::string errorMessage){
+        // CONNECTION FAILED
+        cout << errorCode << " : " << errorMessage << endl;
+    });
+
+    // You should do an input loop so the program will not end immediately:
+    // Because socket listenings are non-blocking.
+    string input;
+    getline(cin, input);
+    while (input != "exit")
+    {
+        tcpSocket.Send(input);
+        getline(cin, input);
+        std::cout << "input = " << input << std::endl;
+    }
+
+    tcpSocket.Close();
+
+    return 0;
+}
